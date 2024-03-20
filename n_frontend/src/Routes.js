9e60cebe23caf62818a1bbfedbb939d25a91d5d5
wahiduzzaman2 <79000023+wahiduzzaman2@users.noreply.github.com>
2024-03-21 05:03:31 +0600

    Create LogIn page

---
+++ n_frontend/src/Routes.js
@@ -0,0 +1,7 @@
+import LoginPage from "../src/pages/Login.jsx"
+import SignupPage from "../src/pages/SignupPage.jsx";
+
+
+export {
+    LoginPage,
+};
---
+++ n_frontend/src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import { useSelector } from 'react-redux';
+// import { useNavigate } from 'react-router-dom';
+
+import Login from "../components/Login/Login.jsx";
+
+const LoginPage = () => {
+  
+  return (
+    <div>
+        <Login/>
+    </div>
+  )
+}
+
+export default LoginPage;
